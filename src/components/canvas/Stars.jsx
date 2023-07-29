ea0de16f788174a2b916c3756d806fb503405fb0
Jon Vincentius <jon.vincentius@mail.utoronto.ca>
2023-07-29 22:59:52 +0700

    Added Contacts Section

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import { useState, useRef, Suspense } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { Points, PointMaterial, Preload } from "@react-three/drei";
+import * as random from "maath/random/dist/maath-random.esm";
 
-const Stars = () => {
+const Stars = (props) => {
+  const ref = useRef();
+  // 5000 particles with 1.2 radius each
+  const [sphere] = useState(() =>
+    random.inSphere(new Float32Array(5000), { radius: 1.2 })
+  );
+
+  // Rotates all the stars frame by frame
+  useFrame((state, delta) => {
+    ref.current.rotation.x -= delta / 10;
+    ref.current.rotation.y -= delta / 15;
+  });
+
   return (
-    <div>Stars</div>
-  )
-}
+    // Generates all the stars within a sphere
+    <group rotation={[0, 0, Math.PI / 4]}>
+      <Points ref={ref} positions={sphere} stride={3} frustumCulled {...props}>
+        <PointMaterial
+          transparent
+          color="#ffffff"
+          size={0.002}
+          sizeAttenuation={true}
+          depthWrite={false}
+        />
+      </Points>
+    </group>
+  );
+};
 
-export default Stars
+const StarsCanvas = () => {
+  return (
+    // use "absolute" to position an element outside of the normal flow of the document, causing
+    // neighboring elements to act as if the element doesn’t exist.
+    // setting "z-index" as -1 makes it appear behind all other elements
+    <div className="w-full h-full absolute inset-0 z-[-1]">
+      <Canvas camera={{ position: [0, 0, 1] }}>
+        <Suspense fallback={null}>
+          <Stars />
+        </Suspense>
+
+        <Preload all />
+      </Canvas>
+    </div>
+  );
+};
+
+export default StarsCanvas;
